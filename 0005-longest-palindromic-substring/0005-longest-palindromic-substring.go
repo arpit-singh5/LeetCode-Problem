da8860f184bdaf348d3a44c12fad3f84be32e975
Arpit <arpitsinghrajput0909@gmail.com>
2026-01-17 09:52:50 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 15 ms (41.39%), Space: 9.7 MB (14.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -1,27 +1,46 @@
 func longestPalindrome(s string) string {
-    maxStart := 0
-    maxLen := 1
-    for i := 0; i < len(s); i++ {
-        for j := i+1; j <= len(s); j++ {
-            substring := s[i:j]
-           
-            if isPalindrome(substring) && len(substring) > maxLen {
-                maxLen = len(substring)
-                maxStart = i
-            }
+    if len(s) == 0 {
+        return ""
+    }
+    C := 0
+    R := 0  
+    mString := ""
+
+    mString += "#"
+    for _,v := range s {
+        mString += string(v) + "#"
+    }
+    n := len(mString)
+    p := make([]int, n)
+
+    for i,_ := range mString {
+        mirror := 2*C - i
+
+        if i < R {
+            p[i] = mini(R-i, p[mirror])
+        }
+        for i+p[i]+1 < n && i-p[i]-1 >= 0 && mString[i+p[i]+1] == mString[i-p[i]-1] {
+            p[i]++
+        }
+        if i+p[i] > R {
+            C = i
+            R = i+ p[i]
         }
     }
-    return s[maxStart:maxLen+maxStart]
+    maxLen := 0
+    Center := 0
+    for i := 1; i < n-1; i++ {
+        if p[i] > maxLen {
+            maxLen = p[i]
+            Center = i
+        }
+    }
+    start := (Center - maxLen)/2
+    return s[start : start+maxLen]
 }
-
-func isPalindrome(s string) bool {
-    left, right := 0, len(s)-1
-    for left < right {
-    if s[left] != s[right] {
-        return false
+func mini (a,b int) int {
+    if a>b {
+        return b
     }
-    left++
-    right--
-    }
-    return true
+    return a
 }
